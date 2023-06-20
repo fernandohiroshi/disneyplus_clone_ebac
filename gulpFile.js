a84018f4b11acb385c251cb13d40eca0a899c2ad
Fernando Hiroshi <fernandohiroshidev@gmail.com>
2023-06-20 15:51:14 -0300

    criando o hero e importanto a fonte avenir

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,8 @@
-// npm instalados
+// npm instalados:
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+// npm i --save-dev gulp-imagemin@7.1.0
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -8,8 +10,15 @@ function styles() {
     .pipe(gulp.dest('./dist/css')) // destino dos arquivos comprimidos
 }
 
-exports.default = styles;
-// para modificar automaticamente alterações do projeto
+
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin()) // para comprimir imagens
+    .pipe(gulp.dest('./dist/images')) // destino dos arquivos comprimidos
+}
+
+exports.default = gulp.parallel(styles, images) // executar todos npms
+// para modificar automaticamente alterações do projeto:
 exports.watch = function() {
     // para ativar: npm run build watch
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
